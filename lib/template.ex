bafdbff7af14cddf64b04dea7acb3a6d356831d2
Awlex <no thank you>
2020-12-05 17:42:21 +0000

    Update template

--- lib/template.ex
+++ lib/template.ex
@@ -1,13 +1,22 @@
 defmodule D do
   @behaviour Solution
 
+  @impl true
   def test_result, do: nil
+  @impl true
   def test_result2, do: nil
 
+  @impl true
+  def parse_input(_raw_input) do
+    nil
+  end
+
+  @impl true
   def solve(_input) do
     # Solve here
   end
 
+  @impl true
   def solve2(_input) do
     # Solve here
   end
